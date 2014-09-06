2221d7eb250a855b6ea447b10af8b320b364f778
MrKepzie <alexandre230@msn.com>
2014-09-06 23:38:42 +0200

    endsWith : bug fix

--- SequenceParsing.cpp
+++ SequenceParsing.cpp
@@ -235,7 +235,7 @@ static bool startsWith(const std::string& str,
 
 static bool endsWith(const std::string &str, const std::string &suffix)
 {
-    return ((str.size() < suffix.size()) &&
+    return ((str.size() >= suffix.size()) &&
             (str.compare(str.size() - suffix.size(), suffix.size(), suffix) == 0));
 }
 
