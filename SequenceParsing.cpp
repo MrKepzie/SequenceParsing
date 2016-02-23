342f0ba8393f756638c7dcf58acb64580f94912b
Alexandre Gauthier <alexandre230@msn.com>
2016-02-23 13:32:43 +0100

    Fix s2ws

--- SequenceParsing.cpp
+++ SequenceParsing.cpp
@@ -60,18 +60,17 @@ namespace  {
     
 #ifdef _WIN32
     static std::wstring
-    s2ws(const std::string & s)
+    utf8_to_utf16(const std::string & s)
     {
         
         
-        int len;
-        int slength = (int)s.length() + 1;
-        len = MultiByteToWideChar(CP_ACP, 0, s.c_str(), slength, 0, 0);
-        wchar_t* buf = new wchar_t[len];
-        MultiByteToWideChar(CP_ACP, 0, s.c_str(), slength, buf, len);
-        std::wstring r(buf);
-        delete[] buf;
-        return r;
+        std::wstring native;
+        
+        
+        native.resize(MultiByteToWideChar (CP_UTF8, 0, str.c_str(), -1, NULL, 0) -1);
+        MultiByteToWideChar (CP_UTF8, 0, str.c_str(), str.size(), &native[0], (int)native.size());
+        
+        return native;
         
     } // s2ws
 #endif
