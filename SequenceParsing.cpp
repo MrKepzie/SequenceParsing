4fdb662384026cd74a8d5ce62076d0ae1a0eee98
MrKepzie <alexandre230@msn.com>
2016-10-18 16:59:50 +0200

    utf8_to_utf16: bug fix

--- SequenceParsing.cpp
+++ SequenceParsing.cpp
@@ -64,7 +64,7 @@ namespace  {
     {
         std::wstring native;
         
-        native.resize(MultiByteToWideChar (CP_UTF8, 0, str.data(), str.length(), NULL, 0) -1);
+        native.resize(MultiByteToWideChar (CP_UTF8, 0, str.data(), str.length(), NULL, 0));
         MultiByteToWideChar (CP_UTF8, 0, str.data(), str.length(), &native[0], (int)native.size());
         
         return native;
